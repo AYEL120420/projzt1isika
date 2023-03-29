cd8cbfe647548c447273761a83f28ea9dcc547f2
Mounia TAHAROUCHTE <mouniataharouchte@gmail.com>
2023-03-29 11:52:22 +0200

    bonjour

---
+++ src/main/java/fr/isika/cda24/MonProjet1/MonProjet1/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.isika.cda24.MonProjet1.MonProjet1;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.isika.cda24.MonProjet1.MonProjet1 {
+    requires javafx.controls;
+    exports fr.isika.cda24.MonProjet1.MonProjet1;
+}
